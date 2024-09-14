1761b7ffdb6c2a8f8f476ac0f61cf7886ebeb456
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-09-14 07:30:43 +0200

    Models - Settings, WindowSize, FavoritePlayers

---
+++ WordCupStats/DataLayer/Models/FavoritePlayers.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+namespace DataLayer.Models
+{
+	public class FavoritePlayers
+	{
+		[JsonProperty("men")]
+		public Dictionary<String, List<String>> Men { get; set; } = new Dictionary<string, List<String>>
+		{
+			{"CRO", new List<String>{ "Danijel SUBASIC " } }
+		};
+
+		[JsonProperty("women")]
+		public Dictionary<String, List<String>> Women { get; set; } = new Dictionary<string, List<String>>
+		{
+			{ "USA", new List<string> { "Alex MORGAN", "Megan RAPINOE" } }
+		};
+	}
+}
---
+++ WordCupStats/DataLayer/Models/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace DataLayer.Models
+{
+	public class Settings
+	{
+		[JsonProperty("WindowSize")]
+		public WindowSize WindowSize { get; set; } = WindowSize.Large; 
+
+		[JsonProperty("DataSource")]
+        public String DataSource { get; set; } = "api";
+
+        [JsonProperty("Championship")]
+        public String Championship { get; set; } = "men";
+
+        [JsonProperty("Language")]
+        public String Language { get; set; } = "en";
+
+        [JsonProperty("FavoriteTeamMen")]
+        public String FavoriteTeamMen { get; set; } = "CRO";
+
+        [JsonProperty("FavoriteTeamWomen")]
+        public String FavoriteTeamWomen { get; set; } = "USA";
+
+        [JsonProperty("FavoritePlayers")]
+        public FavoritePlayers favoritePlayers { get; set; } = new FavoritePlayers();
+
+    }
+}
