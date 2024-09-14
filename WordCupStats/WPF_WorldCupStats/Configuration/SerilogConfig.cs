6836b380a1982993138d55af15413a711a6c4514
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-09-14 07:32:25 +0200

    SerilogConfig add

---
+++ WordCupStats/WPF_WorldCupStats/Configuration/SerilogConfig.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DataLayer.Helpers;
+
+namespace WPF_WorldCupStats.Configuration
+{
+	public static class SerilogConfig
+	{
+		public static async Task ConfigureAsync()
+		{
+			//await ClearSeqLogsAsync();
+
+			var logFilePath = FilePathHelper.LogFilePath;
+
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day)
+				.WriteTo.Seq("http://localhost:5341")
+				.CreateLogger();
+		}
+
+		public static async Task ClearSeqLogsAsync()
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				var seqUrl = "http://localhost:5341/api/events";
+
+				var response = await client.DeleteAsync(seqUrl);
+
+				if (response.IsSuccessStatusCode)
+				{
+					Log.Information("Seq logs cleared successfully.");
+				}
+				else
+				{
+					Log.Error($"Failed to clear Seq logs: {response.ReasonPhrase}");
+				}
+			}
+		}
+	}
+}
